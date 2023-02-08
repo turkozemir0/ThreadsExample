660dfc3b2fbe63f11df5b55114d111a1d29d89cb
turkozemir0 <turkozemir@gmail.com>
2023-02-08 21:14:09 +0300

    Initial commit

---
+++ ismailhocaders2soru/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace ismailhocaders2soru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(306, 260);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(409, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(306, 260);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(809, 12);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(306, 260);
+            this.listBox3.TabIndex = 2;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(1234, 12);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(306, 260);
+            this.listBox4.TabIndex = 3;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 16;
+            this.listBox5.Location = new System.Drawing.Point(616, 301);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(306, 260);
+            this.listBox5.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1234, 474);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ogrenci no";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(497, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "subeler";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(998, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "bolumler";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1380, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "derskodu";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(981, 545);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "doğum tarihi doğum yeri";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1672, 573);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ismailhocaders2soru/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ismailhocaders2soru
+{
+    public partial class Form1 : Form
+    {
+        Thread islem1;
+        Thread islem2;
+        Thread islem3;
+        Thread islem4;
+        Thread islem5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1.CheckForIllegalCrossThreadCalls = false;
+            islem1 = new Thread(new ThreadStart(ogrenciuret));
+            
+            islem2 = new Thread(new ThreadStart(subeuret));
+            
+            islem3 = new Thread(new ThreadStart(bolumuret));
+            islem1.Start();
+            islem2.Start();
+            islem3.Start();
+        }
+        int ogrno = 707001;
+        string[] subeler = { "a", "b", "c", "d", "e" };
+        string[] bolumler = { "elektronik", "bilgisayar", "elektrik", "matematik" };
+        string[] bolumlerinkodlari = { "elektronik101", "bilgisayar101", "elektrik101", "matematik101" };
+      //  string[] dersadlari = { "Adana 25.10.2001", "Balikesir 10.10.2000", "Erzurum 05.05.1999", "Adıyaman 01.01.1994" };
+        string[] sehirler = { "Adana", "Balikesir", "Şırnak", "Adıyaman", "Hakkari", "Diyarbakır", "Muğla", "Aydın" };
+        int[] dogumtarihleri = { 2021, 1999, 2000, 2003, 2004 };
+        
+        //listbox5 dyeri dogumtarihi
+        void ogrenciuret()
+        {
+            while (true)
+            {
+                listBox1.Items.Add("2021"+ogrno++);
+                Thread.Sleep(1000);
+            }
+        }
+        void subeuret()
+        {
+            while (true)
+            {
+                foreach (var item in subeler)
+                {
+                    listBox2.Items.Add(item);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        
+        void bolumuret()
+        {
+            while (true)
+            {
+                Random rnd = new Random();
+                int sayi = rnd.Next(0, bolumler.Length);
+                Random rnd2 = new Random();
+                int sayi2 = rnd.Next(0, sehirler.Length);
+                Random rnd3 = new Random();
+                int sayi3 = rnd.Next(0, dogumtarihleri.Length);
+                listBox3.Items.Add(bolumler[sayi]);
+                listBox4.Items.Add(bolumlerinkodlari[sayi]);
+                
+                listBox5.Items.Add(sehirler[sayi2]+" "+dogumtarihleri[sayi3]);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            islem1.Abort();
+            islem2.Abort();
+            islem3.Abort();
+
+        }
+
+
+
+        //1 buton 2 listbox form load list box 100de n itibaren 1 er artarak listbox2de ise 115ten baslayip 15 er artsın buton tıkaldıgında isslem dursun
+        //listbox 1 ve 2 deki sayilar esdeger hareket etmeli
+
+        //5 listbox 1 button form load edildiği zaman
+        //listbox1 içinde sayı değerleri 2021707001den baslasın ve 1 er 1 er artsın.
+        //list box 2 içinde şube olarak a,b,c,d,e ye kadar dönsün
+        //listbox3 içinde mekatronik bilgisayar elektrik elektronik bölümleri rastgele görüntülensin
+        //listbox4 de gelen bölümegöre ders adı görüntülensin
+    }
+}
